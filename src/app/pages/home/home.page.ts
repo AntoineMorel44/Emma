b258eeb16f8fe7d1a7f9f23ce56340a2968b7b78
Antoine Morel <antoinemorel2@hotmail.fr>
2020-11-16 19:44:33 +0100

    Feat/ Nouvelle version

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, Injector } from '@angular/core';
 import * as moment from 'moment';
 // Importing bootstrap "node_modules/bootstrap/dist/css/bootstrap.min.css"
-              
+
 enum Page {
   INTRODUCTION,
   DISCUSS,
@@ -12,6 +12,10 @@ export interface Challenge {
   name: string;
   dateFin: string;
   tempsRestant: string;
+}
+export interface Advice {
+  title: string;
+  usefullResponses: string[];
 }
 export interface Atelier {
   name: string;
@@ -37,10 +41,11 @@ export class HomePage {
   botAskingAdviceIndex = 0;
   botAskingChallengeIndex = 0;
   storageVersion: string;
-  appVersion = '0.03';
+  appVersion = '0.04';
 
-  myAdvices: any[] = [];
+  myAdvices: Advice[] = [];
   myChallenges: Challenge[] = [];
+  usefullResponses: string[];
 
   dateLastOpeningApplication;
 
@@ -51,23 +56,34 @@ export class HomePage {
 
   ngOnInit() {
     this.initLocalStorage();
-    
-    if(!Array.isArray(this.ateliersPlanning) || !this.ateliersPlanning.length) {
+
+    if (!Array.isArray(this.ateliersPlanning) || !this.ateliersPlanning.length) {
       this.initAteliersPlanning();
-    } else if(!moment().isSame(this.dateLastOpeningApplication, 'day')) {
+    } else if (!moment().isSame(this.dateLastOpeningApplication, 'day')) {
       this.resetAtelierDone();
     }
 
     this.saveAllToLocalStorage();
-  } 
+
+    this.initLngDetector();
+
+    // OR
+    // const lngDetector = new (require('languagedetect'));
+
+  }
+
+  private initLngDetector() {
+    const LanguageDetect = require('languagedetect');
+    const lngDetector = new LanguageDetect();
+  }
 
   initAteliersPlanning() {
-    const discussion: Atelier = {name: 'Vous êtes en souffrance émotionnellement et vous souhaitez être apaisé', css: 'btn3', typeAtelier: 'discussion', done: false};
+    const discussion: Atelier = { name: 'Vous êtes en souffrance émotionnellement et vous souhaitez être apaisé', css: 'btn3', typeAtelier: 'discussion', done: false };
     // const discussion2: Atelier = {name: 'Comprenez-vous et explorez les solutions : Vos peurs', css: 'btn3', typeAtelier: 'discussion', done: false};
-    const challenge: Atelier = {name: 'Changer vos habitudes : challenge du jour', css: 'btn4', typeAtelier: 'challenge', done: false};
+    const challenge: Atelier = { name: 'Changer vos habitudes : challenge du jour', css: 'btn4', typeAtelier: 'challenge', done: false };
     // const challenge2: Atelier = {name: 'Challenge du jour #2', css: 'btn4', typeAtelier: 'challenge', done: false};
     // const challenge3: Atelier = {name: 'Challenge du jour #3', css: 'btn4', typeAtelier: 'challenge', done: false};
-    const reves: Atelier = {name: 'Cap sur vos rêves !', css: 'btn2', typeAtelier: 'reves', done: false};
+    const reves: Atelier = { name: 'Cap sur vos rêves !', css: 'btn2', typeAtelier: 'reves', done: false };
     // const reves2: Atelier = {name: 'Vos rêves en marche #2', css: 'btn2', typeAtelier: 'reves', done: false};
 
     this.ateliersPlanning = [];
@@ -83,21 +99,25 @@ export class HomePage {
   }
 
   initLocalStorage() {
+    this.storageVersion = localStorage.getItem('storageVersion');
     this.notificationsAdivce = +localStorage.getItem('notificationsAdivce');
     this.notificationsChallenge = +localStorage.getItem('notificationsChallenge');
     this.ateliersPlanning = JSON.parse(localStorage.getItem('ateliersPlanning'));
-    this.myAdvices = JSON.parse(localStorage.getItem('myAdvices'));
     this.myChallenges = JSON.parse(localStorage.getItem('myChallenges'));
     this.dateLastOpeningApplication = localStorage.getItem('dateLastOpeningApplication');
-    this.storageVersion = localStorage.getItem('storageVersion');
 
-    if(this.storageVersion !== this.appVersion) {
+    if (this.storageVersion === '0.03') {
+      this.myAdvices = [];
+      (JSON.parse(localStorage.getItem('myAdvices')) as string[]).forEach(advice => this.myAdvices.push({title:advice,usefullResponses: []}));
+    } else if (this.storageVersion !== this.appVersion) {
       this.notificationsAdivce = 0;
       this.notificationsChallenge = 0;
       this.ateliersPlanning = [];
       this.myAdvices = [];
       this.myChallenges = [];
       this.dateLastOpeningApplication = moment().format();
+    } else {
+      this.myAdvices = JSON.parse(localStorage.getItem('myAdvices'));
     }
   }
 
@@ -132,16 +152,23 @@ export class HomePage {
     const messageList = <HTMLElement>messagesList.querySelector('#messageList');
     setTimeout(() => messageList.scrollTop = 0, 1500);
 
-    
+
     const icon = <HTMLElement>dfMessenger.querySelector('#widgetIcon');
     console.log('icon', icon);
     icon.style.display = 'none';
 
-    
+
     const chatWrapper = <HTMLElement>dfMessengerChat.querySelector('.chat-wrapper');
     chatWrapper.style.right = '0px';
     chatWrapper.style.bottom = '0px';
     chatWrapper.style.width = '100%';
+
+
+    const dfMessengerUserInput = dfMessengerChat.querySelector('df-messenger-user-input').shadowRoot;
+    const sendIcon = <HTMLElement>dfMessengerUserInput.querySelector('#sendIcon');
+    console.log('dfMessengerUserInput.performButtonClickActions_', dfMessengerUserInput)
+
+
   }
 
   subscribeToMessengerEvents() {
@@ -149,20 +176,26 @@ export class HomePage {
 
     const dfMessenger = document.querySelector('df-messenger');
     window.addEventListener('df-response-received', function (event: any) {
-      
-      // if(event.detail.response.queryResult.intent.isFallback && _this.lastRequestSent.length > 0 &&  _this.lastRequestSent[_this.lastRequestSent.length-1].queryInput.text.text.split(" ").length - 1 < 1) {
-      //   const dfMessenger = document.querySelector('df-messenger') as any;
-      //   dfMessenger.renderCustomText('Je ne suis pas sûr d\'avoir compris votre dernière réponse...');
-      //   dfMessenger.performButtonClickActions_('test');
-      // }
-
       const clone = JSON.parse(JSON.stringify(event.detail.response));
       console.log('_this.lastResponseReceived', event);
       _this.lastResponseReceived.push(clone);
       _this.handleResponse(clone);
+
+      if(event.detail.response.queryResult.intent.isFallback && _this.lastRequestSent.length > 0) {
+        _this.usefullResponses.push(_this.lastRequestSent[_this.lastRequestSent.length-1].queryInput.text.text);
+    }
     });
 
     dfMessenger.addEventListener('df-request-sent', function (event: any) {
+      const LanguageDetect = require('languagedetect');
+      const lngDetector = new LanguageDetect();
+      const request = event.detail.requestBody.queryInput.text.text;
+
+      if (!(lngDetector.detect(request, 5)).find(l => l[0] === 'french') && (request).toLocaleLowerCase() !== 'ok') {
+        const dfMessenger = document.querySelector('df-messenger') as any;
+        dfMessenger.renderCustomText('Je ne suis pas sûre d\'avoir compris votre dernière réponse...');
+      }
+
       const clone = JSON.parse(JSON.stringify(event.detail.requestBody));
       _this.lastRequestSent.push(clone);
       console.log('_this.lastRequestSent', event.detail.requestBody);
@@ -181,22 +214,24 @@ export class HomePage {
       const fulfillmentMessages = response.queryResult.fulfillmentMessages as Array<any>;
 
       fulfillmentMessages.forEach(message => {
-        if(message && message.payload && message.payload.advice) {
-          const advice = this.lastRequestSent[this.botAskingAdviceIndex].queryInput.text.text;
+        if (message && message.payload && message.payload.advice) {
+          const lastResponse: string = this.lastRequestSent[this.botAskingAdviceIndex].queryInput.text.text;
+          const advice: Advice = {title: lastResponse, usefullResponses: JSON.parse(JSON.stringify(this.usefullResponses))};
+          this.usefullResponses = [];
           this.myAdvices.push(advice);
           this.notificationsAdivce++;
           this.ateliersPlanning[0].done = true;
           this.saveAllToLocalStorage();
-        } else if(message && message.payload && message.payload.challenge) {
+        } else if (message && message.payload && message.payload.challenge) {
           const challengeName = this.lastRequestSent[this.botAskingChallengeIndex].queryInput.text.text;
-          let challenge: Challenge = {name: challengeName, dateFin: moment().add(1, 'days').format(), tempsRestant: ''};
+          let challenge: Challenge = { name: challengeName, dateFin: moment().add(1, 'days').format(), tempsRestant: '' };
           this.myChallenges.push(challenge);
           this.notificationsChallenge++;
           this.ateliersPlanning[0].done = true;
           this.saveAllToLocalStorage();
-        } else if(message && message.payload && message.payload.askingAdvice) {
+        } else if (message && message.payload && message.payload.askingAdvice) {
           this.botAskingAdviceIndex = this.lastRequestSent.length;
-        } else if(message && message.payload && message.payload.askingChallenge) {
+        } else if (message && message.payload && message.payload.askingChallenge) {
           this.botAskingChallengeIndex = this.lastRequestSent.length;
         }
       });
@@ -205,40 +240,46 @@ export class HomePage {
 
   initMessengerWhenOpened() {
     const messenger = document.querySelector('df-messenger');
-    if(messenger) {
-      console.log('messenger',messenger)
+    if (messenger) {
+      console.log('messenger', messenger)
       this.dfMessengerInit();
     } else {
       setTimeout(() => this.initMessengerWhenOpened(), 100);
     }
   }
 
-  startChat(chatType: string) {    
-    this.chat = '';
-    setTimeout(() => {
-      this.chat = chatType;
-      this.initMessengerWhenOpened();
-    }, 100);
+  startChat(chatType: string) {
+    this.usefullResponses = [];
+    if(chatType === this.chat) {
+      const messenger = document.querySelector('df-messenger') as any;
+      messenger.toggleExpandAttribute_();
+    } else {
+      this.chat = '';
+      setTimeout(() => {
+        this.chat = chatType;
+        this.initMessengerWhenOpened();
+      }, 100);
+    }
 
     // if(chatType === 'day' &&  this.ateliersPlanning[0].typeAtelier === chatType) {
     //   // Popup
     // } else if(chatType === 'reves' && this.revesLocked) {
     //   // Popup
     // } else {
-      
+
     // }
   }
 
   setView(page: Page) {
     if (this.myChallenges.length === 0 && page === Page.CHALLENGE) {
       // Ne rien faire pour l'instant
-    } else if(this.myAdvices.length === 0 && page === Page.ADVICE) {
+    } else if (this.myAdvices.length === 0 && page === Page.ADVICE) {
       // Ne rien faire pour l'instant
     } else {
-      if(page === Page.ADVICE) {
+      if (page === Page.ADVICE) {
         this.notificationsAdivce = 0;
         this.saveAllToLocalStorage();
-      } else if(page === Page.CHALLENGE) {
+      } else if (page === Page.CHALLENGE) {
         this.notificationsChallenge = 0;
         this.computeTempsRestantOfChallenges();
         this.saveAllToLocalStorage();
@@ -246,12 +287,12 @@ export class HomePage {
       this.scrollToTop();
       this.view = page;
     }
-    
+
   }
 
   computeTempsRestantOfChallenges() {
     this.myChallenges.forEach(challenge => {
-      if(moment().isSameOrAfter(challenge.dateFin)) {
+      if (moment().isSameOrAfter(challenge.dateFin)) {
         challenge.tempsRestant = 'Challenge terminé ✔'
       } else {
         const tempsRestant = moment.utc(moment(challenge.dateFin).diff(moment()));
